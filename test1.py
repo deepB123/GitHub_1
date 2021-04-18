60aada1ed30dc904de4c1d0efd1c4c8390c0ff8d
Deepa <b1234deep@gmail.com>
2021-04-18 09:49:54 +0000

    add a test1 python file

---
+++ test1.py
@@ -0,0 +1,19 @@
+import re
+
+content= ['hello Judy, you are invited to party on 22.04.2021',
+          'hey Agatha, you are invited to party on 22.04.2021',
+          'hi Chris, you are invited to party on 22.04.2021',
+          ' let\'s celebrate World Earth day '
+          ]
+
+p0 = re.compile(r'^(\S+)\s+(?P<name>(\w+)),\s+((\w+|\s+)*)(?:22.04.2021)$')
+
+invitee = []
+for i in content:
+    m = p0.search(i)
+    if m:
+        var = (m.group('name'))
+        invitee.append(var)
+    
+
+print('invitees --', invitee)
